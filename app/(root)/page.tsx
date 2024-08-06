ba5688e480ee6063e80e60d16c6eb4caf83d3886
Ritesh Raj Pandit <41156634+riteshrajpandit@users.noreply.github.com>
2024-08-06 22:26:51 +0545

    implement header as props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+
+import HeaderBox from '@/components/HeaderBox'
 
 const Home = () => {
+  const loggedIn = {firstName:'Ritesh'};
   return (
-    <div> Home</div>
+     
+    <section className= "home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
